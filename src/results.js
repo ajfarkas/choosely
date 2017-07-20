7e0b907a59ba90dd3676c4e51ab49a42283b9314
AJ Farkas <farkas.aj@gmail.com>
2017-07-20 16:52:16 -0700

    Highlight score differential

--- src/results.js
+++ src/results.js
@@ -10,7 +10,9 @@ function displayNames() {
         ),
         partnerNames = names.sort((a,b) =>
           Data.firstnames[a][Data.user.partner].score < Data.firstnames[b][Data.user.partner].score
-        )
+        ),
+        youScoreMax = Math.max(...names.map(name => Data.firstnames[name][Data.user.user].score)),
+        partnerScoreMax = Math.max(...names.map(name => Data.firstnames[name][Data.user.partner].score))
   let you = undefined,
       partner = undefined,
       youName = undefined,
@@ -20,12 +22,16 @@ function displayNames() {
     you = document.createElement('li')
     youName = Data.firstnames[userNames[i]]
     you.className = 'result'
+    you.style.backgroundColor = `rgba(255,255,255,${youName[Data.user.user].score/youScoreMax})`
+    you.dataset.id = userNames[i]
     you.innerText = `${youName.name}: ${youName[Data.user.user].score}`
     userList.append(you)
 
     partner = document.createElement('li')
     partnerName = Data.firstnames[partnerNames[i]]
     partner.className = 'result'
+    partner.style.backgroundColor = `rgba(255,255,255,${partnerName[Data.user.partner].score/partnerScoreMax})`
+    partner.dataset.id = partnerNames[i]
     partner.innerText = `${partnerName.name}: ${partnerName[Data.user.partner].score}`
     partnerList.append(partner)
   }
